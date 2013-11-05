70fd5df1f33f5717c573bad484d8b25bec710a3b
Chris Samuelson <chris.sam55@gmail.com>
2013-11-05 20:43:18 +0000

    Created misc utilities header file.  Currently only includes debug conditional printing.

---
+++ util/jMisc.h
@@ -0,0 +1,10 @@
+#ifdef  DEBUG
+#define DEBUG_TEST 1
+#else
+#define DEBUG_TEST 0
+#endif
+
+#define debug_print(fmt)\
+        do{if(DEBUG_TEST) fprintf(stderr, "%s:%d:%s() " fmt, __FILE__,\
+                            __LINE__,__func__);} while(0)
+
