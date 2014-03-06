c16c9445c74547c313b8571c9089b6d3711eaaf2
Chris Samuelson <chris.sam55@gmail.com>
2014-03-06 17:27:37 -0500

    Updated misc header.

--- util/jMisc.h
+++ util/jMisc.h
@@ -1,10 +1,22 @@
+#ifndef __MISC_H__
+#define __MISC_H__
+
 #ifdef  DEBUG
 #define DEBUG_TEST 1
 #else
 #define DEBUG_TEST 0
 #endif
 
+namespace Jing{
+
+typedef unsigned int size_t;
+typedef unsigned int index_t;
+
 #define debug_print(fmt)\
         do{if(DEBUG_TEST) fprintf(stderr, "%s:%d:%s() " fmt, __FILE__,\
                             __LINE__,__func__);} while(0)
+
+}
+
+#endif
 
